646f60149d789370f874b9f5c68cda1e874133de
Shabbir Rayeen <sawone2893@gmail.com>
2026-01-16 14:49:57 +0530

    /**  * Topic covered in this  * Handling dialogs  * Grouping tests  * Hooks  * Tags:  */

--- tests/TestDialogs.spec.js
+++ tests/TestDialogs.spec.js
@@ -1,5 +1,12 @@
 import { test, expect } from "@playwright/test";
 //Here we handles diagloga and implements the grouping concept.
+/**
+ * Topic covered in this
+ * Handling dialogs
+ * Grouping tests
+ * Hooks
+ * Tags:
+ */
 test.describe("Test Dialogs", async () => {
   const url = "https://testautomationpractice.blogspot.com/";
 
@@ -10,18 +17,20 @@ test.describe("Test Dialogs", async () => {
     await page.close();
   });
 
-  test("Simple Alert", async ({ page }) => {
-    page.on("dialog", (dialog) => dialog.accept());
-    await page.getByRole("button", { name: "Simple Alert" }).click();
-  });
+  test(
+    "Simple Alert",{tag: "@sa"},async ({ page }) => {
+      page.on("dialog", (dialog) => dialog.accept());
+      await page.getByRole("button", { name: "Simple Alert" }).click();
+    }
+  );
 
-  test("Confirmation Alert", async ({ page }) => {
+  test("Confirmation Alert",{tag: "@ca"}, async ({ page }) => {
     page.on("dialog", (dialog) => dialog.accept());
     await page.getByRole("button", { name: "Confirmation Alert" }).click();
     expect(page.locator("#demo")).toContainText("You pressed OK!");
   });
 
-  test("Prompt Alert", async ({ page }) => {
+  test("Prompt Alert",{tag: "@pa"}, async ({ page }) => {
     page.on("dialog", (dialog) => dialog.accept("Shabbir Rayeen"));
     await page.getByRole("button", { name: "Prompt Alert" }).click();
     expect(page.locator("#demo")).toContainText("Shabbir Rayeen");
