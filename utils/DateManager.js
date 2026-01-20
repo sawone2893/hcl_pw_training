094a4521e6205803f957d1a6b38f59204ebb5f75
Shabbir Rayeen <sawone2893@gmail.com>
2026-01-20 21:24:20 +0530

    Updated DateManager utiltiy methods

--- utils/DateManager.js
+++ utils/DateManager.js
@@ -1,12 +1,45 @@
-export function getMonthIndex(monthName) {
+export function getFullMonthIndex(monthName) {
   const months = [
-    "january", "february", "march", "april", "may", "june",
-    "july", "august", "september", "october", "november", "december"
+    "january",
+    "february",
+    "march",
+    "april",
+    "may",
+    "june",
+    "july",
+    "august",
+    "september",
+    "october",
+    "november",
+    "december",
   ];
- 
-  return months.indexOf(monthName.toLowerCase())+1;
+
+  return months.indexOf(monthName.toLowerCase()) + 1;
 }
 
-export function getCurrentMonthIndex(){
+export function getShortMonthIndex(monthName) {
+  const months = [
+    "jan",
+    "feb",
+    "mar",
+    "apr",
+    "may",
+    "jun",
+    "jul",
+    "aug",
+    "sept",
+    "oct",
+    "nov",
+    "dec",
+  ];
+
+  return months.indexOf(monthName.toLowerCase()) + 1;
+}
+
+export function getCurrentMonthIndex() {
   return new Date().getMonth() + 1;
+}
+
+export function getCurrentYear() {
+  return new Date().getFullYear();
 }
