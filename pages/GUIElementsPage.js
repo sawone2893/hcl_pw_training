5014676831bc83e35575855b13de59fe6a4234ce
Shabbir Rayeen <sawone2893@gmail.com>
2026-01-20 21:24:48 +0530

    Added new method to GUIElementsPage

--- pages/GUIElementsPage.js
+++ pages/GUIElementsPage.js
@@ -1,10 +1,12 @@
-import {BasePage} from "../pages/BasePage";
-export class GUIElementsPage extends BasePage{
+import { BasePage } from "../pages/BasePage";
+export class GUIElementsPage extends BasePage {
   constructor(page) {
     super(page);
   }
 
-  async navigateToPage(){
+  pageObject = {};
+
+  async navigateToPage() {
     await this.actions.navigateTo("/");
   }
   async enterName(name) {
@@ -21,17 +23,41 @@ export class GUIElementsPage extends BasePage{
     await this.actions.clickElement("css", `#${gender.toLowerCase()}`);
   }
 
-  async selectDay(name){
-    await this.actions.selectRadioCheckbox("css",`#${name.toLowerCase()}`)
+  async selectDay(name) {
+    await this.actions.selectRadioCheckbox("css", `#${name.toLowerCase()}`);
   }
 
-  async selectCountry(name){
-    await this.actions.selectDropDown("#country",name);
+  async selectCountry(name) {
+    await this.actions.selectDropDown("#country", name);
   }
-  async selectColors(names){
-    await this.actions.selectDropDown("#colors",names);
+  async selectColors(names) {
+    await this.actions.selectDropDown("#colors", names);
   }
-  async selectAnimals(names){
-    await this.actions.selectDropDown("#animals",names);
+  async selectAnimals(names) {
+    await this.actions.selectDropDown("#animals", names);
+  }
+
+  async selectDatePicker1(dateValue) {
+    await this.selectDateFromDatePicker(dateValue, "#datepicker");
+  }
+
+  async selectDatePicker2(dateValue) {
+    await this.selectDateFromDatePickerWithMonthYearDropdown(dateValue);
+  }
+
+  async selectStartDate(dateValue) {
+    await this.actions.typeText("placeholder", "Start Date", dateValue);
+  }
+
+  async selectEndDate(dateValue) {
+    await this.actions.typeText("placeholder", "End Date", dateValue);
+  }
+
+  async clickSubmitBtn(){
+    await this.actions.clickElement("css", `button.submit-btn`);
+  }
+
+  getMessageLocator(){
+    return this.actions.getLocator("css","#result");
   }
 }
