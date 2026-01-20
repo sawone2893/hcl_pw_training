a27c28b21114f1de62852b57306b9d01c5274504
Shabbir Rayeen <sawone2893@gmail.com>
2026-01-20 14:54:52 +0530

    Updated spec as POM implementation

--- tests/TestDialogs.spec.js
+++ tests/TestDialogs.spec.js
@@ -1,4 +1,4 @@
-import { test, expect } from "@playwright/test";
+import { test } from "../fixtures/PageFixtures";
 //Here we handles diagloga and implements the grouping concept.
 /**
  * Topic covered in this
@@ -8,31 +8,26 @@ import { test, expect } from "@playwright/test";
  * Tags:
  */
 test.describe("Test Dialogs", async () => {
-  const url = "https://testautomationpractice.blogspot.com/";
-
-  test.beforeEach(async ({ page }) => {
-    await page.goto(url);
-  });
-  test.afterEach(async ({ page }) => {
-    await page.close();
+  test("Simple Alert", { tag: "@sa" }, async ({ dialogPage }) => {
+    await dialogPage.navigateToDialog();
+    await dialogPage.acceptSimpleAlert();
   });
 
-  test(
-    "Simple Alert",{tag: "@sa"},async ({ page }) => {
-      page.on("dialog", (dialog) => dialog.accept());
-      await page.getByRole("button", { name: "Simple Alert" }).click();
-    }
-  );
-
-  test("Confirmation Alert",{tag: "@ca"}, async ({ page }) => {
-    page.on("dialog", (dialog) => dialog.accept());
-    await page.getByRole("button", { name: "Confirmation Alert" }).click();
-    expect(page.locator("#demo")).toContainText("You pressed OK!");
+  test("Confirmation Alert", { tag: "@ca" }, async ({ dialogPage, assert }) => {
+    await dialogPage.navigateToDialog();
+    await dialogPage.acceptConfirmationAlert();
+    assert.validatePartialText(
+      dialogPage.getVisibleMessageElement(),
+      "You pressed OK!",
+    );
   });
 
-  test("Prompt Alert",{tag: "@pa"}, async ({ page }) => {
-    page.on("dialog", (dialog) => dialog.accept("Shabbir Rayeen"));
-    await page.getByRole("button", { name: "Prompt Alert" }).click();
-    expect(page.locator("#demo")).toContainText("Shabbir Rayeen");
+  test("Prompt Alert", { tag: "@pa" }, async ({ dialogPage, assert }) => {
+    await dialogPage.navigateToDialog();
+    await dialogPage.acceptPromptAlert("Shabbir");
+    assert.validatePartialText(
+      dialogPage.getVisibleMessageElement(),
+      "Shabbir",
+    );
   });
 });
