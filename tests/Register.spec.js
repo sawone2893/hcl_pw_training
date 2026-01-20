1411dc87268b8cabd3e51199d4905aa0e530f659
Shabbir Rayeen <sawone2893@gmail.com>
2026-01-21 00:22:29 +0530

    Updated testdata for register.spec.js

--- tests/Register.spec.js
+++ tests/Register.spec.js
@@ -10,10 +10,10 @@ test("Register User",{ tag: "@register" }, async ({ gUIElementsPage,assert }) =>
   await gUIElementsPage.selectCountry("india");
   await gUIElementsPage.selectColors(["green", "blue"]);
   await gUIElementsPage.selectAnimals(["cat", "cheetah"]);
-  await gUIElementsPage.selectDatePicker1("2 Janauary 2026");
+  await gUIElementsPage.selectDatePicker1("2 January 2026");
   await gUIElementsPage.selectDatePicker2("12 Jan 2026");
   await gUIElementsPage.selectStartDate("2026-01-14");
-  await gUIElementsPage.selectStartDate("2026-01-15");
+  await gUIElementsPage.selectEndDate("2026-01-15");
   await gUIElementsPage.clickSubmitBtn();
   assert.validateExactText(gUIElementsPage.getMessageLocator(),"You selected a range of 1 days.");
 });
