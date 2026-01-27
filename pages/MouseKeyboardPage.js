689ab158a926b203902d9854d3aaca206d40c186
Shabbir Rayeen <sawone2893@gmail.com>
2026-01-27 22:29:28 +0530

    Added new page: MouseKeyboardPage

---
+++ pages/MouseKeyboardPage.js
@@ -0,0 +1,13 @@
+import { BasePage } from "../pages/BasePage";
+export class MouseKeyboardPage extends BasePage{
+  constructor(page){
+    super(page);
+  }
+  pageLocators={
+
+  };
+
+  async hoverPointMe(){
+    
+  }
+}
