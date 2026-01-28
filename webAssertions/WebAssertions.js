ea014f24b3544fd7e5cfc44d49ced47678dbda28
Shabbir Rayeen <sawone2893@gmail.com>
2026-01-28 20:07:05 +0530

    Added new method to validate based on text values.

--- webAssertions/WebAssertions.js
+++ webAssertions/WebAssertions.js
@@ -3,7 +3,14 @@ export class WebAssertion {
   validatePartialText(locator, expectedText) {
     expect(locator).toContainText(expectedText);
   }
+  validatePartialText(actualtext, expectedText) {
+    expect(actualtext).toContain(expectedText);
+  }
   validateExactText(locator, expectedText) {
     expect(locator).toHaveText(expectedText);
+  }
+
+  validateExactText(actualtext, expectedText) {
+    expect(actualtext).toBe(expectedText);
   }
 }
