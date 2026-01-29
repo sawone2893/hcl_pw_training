d74b5842883675854431ad30605b4f0c3009378e
Shabbir Rayeen <sawone2893@gmail.com>
2026-01-29 11:19:10 +0530

    Updated getStaticWebTableRowByBookName function

--- pages/WebTablePage.js
+++ pages/WebTablePage.js
@@ -1,22 +1,29 @@
 import { BasePage } from "./BasePage";
-export class WebTablePage extends BasePage{
-  constructor(page){
+export class WebTablePage extends BasePage {
+  constructor(page) {
     super(page);
   }
-  pageLocators={
-    
-  };
+  pageLocators = {};
   async navigateToWebTablePage() {
     await this.actions.navigateTo("/");
   }
-  async getStaticWebTableRowByBookName(bookName){
-    const rows=await this.getTableRows("name","BookTable");
-    const row =await this.getSpecificRow(rows,bookName);
-    const cells=await this.getRowCells(row,"td");
-    console.log(`BookName: ${this.actions.getText(cells[0])}`);
-    console.log(`Author: ${this.actions.getText(cells[1])}`);
-    console.log(`Subject: ${this.actions.getText(cells[2])}`);
-    console.log(`Price: ${this.actions.getText(cells[3])}`);
+  async getStaticWebTableRowByBookName(bookName) {
+    const rows = await this.getTableRows("name", "BookTable");
+    console.log(`Rows Count: ${await this.getRowsCellsCount(rows)}`);
+    const row = await this.getSpecificRow(rows, bookName);
+    const cells = await this.getRowCells(row, "td");
+    console.log(`Cells Count: ${await this.getRowsCellsCount(cells)}`);
+    console.log(
+      `BookName: ${await this.actions.getText(await this.getNthRowCell(cells, 0))}`,
+    );
+    console.log(
+      `Author: ${await this.actions.getText(await this.getNthRowCell(cells, 1))}`,
+    );
+    console.log(
+      `Subject: ${await this.actions.getText(await this.getNthRowCell(cells, 2))}`,
+    );
+    console.log(
+      `Price: ${await this.actions.getText(await this.getNthRowCell(cells, 3))}`,
+    );
   }
-
 }
