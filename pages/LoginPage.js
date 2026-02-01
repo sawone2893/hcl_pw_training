93a3aacd075d80eb349e56f426cb29c04d6f8aae
Shabbir Rayeen <sawone2893@gmail.com>
2026-02-01 10:30:24 +0530

    Updated login page

--- pages/LoginPage.js
+++ pages/LoginPage.js
@@ -1,18 +1,93 @@
 export class LoginPage {
   constructor(page) {
     this.page = page;
-    this.usernameInput=page.getPlaceholder("Username");
-    this.passwordInput=page.getPlaceholder("Password");
-    this.loginBtn=page.locator("//input[@name='login-button']");
+    
+    // Locators
+    this.usernameInput = page.getByPlaceholder("Username");
+    this.passwordInput = page.getByPlaceholder("Password");
+    this.loginBtn = page.locator("//input[@name='login-button']");
+    this.errorMessage = page.locator("[role='alert']");
+    this.rememberMeCheckbox = page.locator("input[type='checkbox']");
+    this.forgotPasswordLink = page.locator("a:has-text('Forgot Password')");
   }
 
-  async navigateToLogin(){
+  /**
+   * Navigate to login page
+   */
+  async navigateToLogin() {
     await this.page.goto("/");
   }
 
-  async appLogin(username,password){
+  /**
+   * Fill username field
+   */
+  async enterUsername(username) {
     await this.usernameInput.fill(username);
+  }
+
+  /**
+   * Fill password field
+   */
+  async enterPassword(password) {
     await this.passwordInput.fill(password);
+  }
+
+  /**
+   * Click login button
+   */
+  async clickLoginButton() {
     await this.loginBtn.click();
+  }
+
+  /**
+   * Complete login with credentials
+   */
+  async appLogin(username, password) {
+    await this.enterUsername(username);
+    await this.enterPassword(password);
+    await this.clickLoginButton();
+  }
+
+  /**
+   * Check if error message is visible
+   */
+  async isErrorMessageVisible() {
+    return await this.errorMessage.isVisible();
+  }
+
+  /**
+   * Get error message text
+   */
+  async getErrorMessage() {
+    return await this.errorMessage.textContent();
+  }
+
+  /**
+   * Click Remember Me checkbox
+   */
+  async clickRememberMe() {
+    await this.rememberMeCheckbox.click();
+  }
+
+  /**
+   * Click Forgot Password link
+   */
+  async clickForgotPassword() {
+    await this.forgotPasswordLink.click();
+  }
+
+  /**
+   * Check if login button is enabled
+   */
+  async isLoginButtonEnabled() {
+    return await this.loginBtn.isEnabled();
+  }
+
+  /**
+   * Wait for login page to load
+   */
+  async waitForLoginPageLoad() {
+    await this.page.waitForLoadState("networkidle");
+    await this.usernameInput.waitFor({ state: "visible" });
   }
 }
